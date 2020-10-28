801c0ed798f4d994e90e1225d533b7f8902c3fb8
Taha Khan <mac@macs-MacBook-Pro.local>
2020-10-29 02:55:42 +0300

    added static root

--- Backend/settings.py
+++ Backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
