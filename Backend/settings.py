af1c0defb5f090541382e43465fbfc38a1e9a036
Taha Khan <mac@macs-MacBook-Pro.local>
2020-10-29 03:33:10 +0300

    Set debug value again

--- Backend/settings.py
+++ Backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.envior.get('DEBUG') == 'True')
+DEBUG = (os.envior.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['pure-sea-87964.herokuapp.com', "127.0.0.1"]
 
