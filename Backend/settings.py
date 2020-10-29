8b821fae0fac4065f8fe34bc67b401351035c8f5
Taha Khan <mac@macs-MacBook-Pro.local>
2020-10-29 04:06:34 +0300

    added django_heroku

--- Backend/settings.py
+++ Backend/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -137,7 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
@@ -164,3 +165,5 @@ CORS_ALLOWED_ORIGINS = [
 # CORS_ORIGIN_WHITELIST = [
 #     'https://localhost:3000',
 # ]
+
+django_heroku.settings(locals())
